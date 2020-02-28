fd6a20d48b4ba72f648f2df6a1b09352bfd14cea
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-28 06:00:20 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -22,13 +22,12 @@ result
 end 
 
 def total_even_pairs(src)
-
   total = 0
   out = 0 
   
   while out < src.length do
     inner = 0
-    while inner < src[out].length do
+    while inner < src[out].count do
       if src[out][0] % 2 == 0 && src[out][1] % 2 == 0
       total += src[out][0] + src[out][1]
       inner += 1 
